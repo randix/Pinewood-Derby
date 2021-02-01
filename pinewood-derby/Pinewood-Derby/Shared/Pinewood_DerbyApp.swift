a689aa620fb601dc54f6292dbb2665dacd9749e3
Randall Dow <randall.dow@fs.utc.com>
2021-01-31 19:28:03 -0800

    Baseline Pinewood-Derby

---
+++ pinewood-derby/Pinewood-Derby/Shared/Pinewood_DerbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinewood_DerbyApp.swift
+//  Shared
+//
+//  Created by Rand Dow on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Pinewood_DerbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
